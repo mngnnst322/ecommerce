a77b7a09882d8c5d36e4bf63350b823a50f45c70
Enkhzul16 <enkhzzull@gmail.com>
2026-04-20 17:11:16 +0800

    provider

--- app/providers/user-provider.tsx
+++ app/providers/user-provider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { User } from "../types";
 
 type UserContextType = {
@@ -10,6 +10,41 @@ type UserContextType = {
 export const UserContext = createContext({} as UserContextType);
 export const UserProvider = ({ children }: { children: React.ReactNode }) => {
   const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    if (user !== null && user.accessToken) {
+      localStorage.setItem("accessToken", user.accessToken);
+    }
+  }, [user]);
+
+  useEffect(() => {
+    if (window) {
+      const token = localStorage.getItem("accessToken");
+      if (token) {
+        let status = 200;
+        fetch("https://dummyjson.com/auth/me", {
+          method: "GET",
+          headers: {
+            Authorization: "Bearer " + token,
+          },
+        })
+          .then((res) => {
+            if (!res.ok) {
+              status = res.status;
+            }
+            return res.json();
+          })
+          .then((data) => {
+            if (status === 200) {
+              setUser(data);
+            } else {
+              setUser(null);
+            }
+          });
+      }
+    }
+  }, []);
+
   return (
     <UserContext.Provider value={{ user, setUser }}>
       {children}
